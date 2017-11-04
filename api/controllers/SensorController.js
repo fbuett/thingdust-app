bf16c28bc3fc0de79823c7d3f23586113a74fbcc
Frank Buettner <frank.buettner@gmail.com>
2017-11-04 10:46:45 +0100

    moved updateOrCreate function from SensorController to SensorModel

--- api/controllers/SensorController.js
+++ api/controllers/SensorController.js
@@ -7,26 +7,4 @@
 
 module.exports = {
 
-  // Update or Insert function
-  updateOrCreate: function(criteria, values, cb) {
-    var self = this; // reference for use by callbacks
-    // If no values were specified, use criteria
-    if (!values) values = criteria.where ? criteria.where : criteria;
-
-    self.findOne(criteria, function (err, result){
-      if (err) 
-        return cb(err, false);
-
-      if (result){
-        // sensor object (=deveui) already exists
-
-        // update sensor object
-        self.update(criteria, values, cb);
-      } else {
-        // create sensor object
-        self.create(values, cb);
-      }
-    });
-  }
-
 };
